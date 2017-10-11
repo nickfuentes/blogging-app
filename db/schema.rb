ab43e97aa2e513bf087e4e1c5004f19c33ddeb9f
Nickolas Fuentes <nickfuentes24@gmail.com>
2017-10-11 09:22:51 -0500

    Created a route, controller, and view for users to see blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005142639) do
+ActiveRecord::Schema.define(version: 20171011135357) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
